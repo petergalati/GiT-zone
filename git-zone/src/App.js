a6c28a129b490def62b89fc9ac677708fcd4769d
os22128 <os22128@bristol.ac.uk>
2024-02-03 15:50:51 +0000

    map exists!!!!!

--- git-zone/src/App.js
+++ git-zone/src/App.js
@@ -1,20 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 import './App.css';
-import BasicMap from "./components/map";
+import badMap from './components/map'
 
 function App() {
-  return (
-    <div className="App">
-        {BasicMap}
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <p>
-          GiT-zone
 
-        </p>
-      </header>
-    </div>
-  );
+    const badmap = badMap()
+    return (
+        <div className="App">
+            <div style={{ height: '50vh', width: '25%' }}>
+                {badmap}
+            </div>
+
+            <header className="App-header">
+                <p>
+                    GiT-zone
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- git-zone/src/components/map.jsx
+++ git-zone/src/components/map.jsx
@@ -1,48 +1,23 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import {APIProvider, Map} from "@vis.gl/react-google-maps";
 
-const containerStyle = {
-    width: '400px',
-    height: '400px'
-};
+const badMap = () => {
+    return (
+        <APIProvider apiKey={"AIzaSyDVGnLbmVRcZ9s82BWKTJ01SipwMv9fDQU"}>
+            <Map
+                width="100%"
+                height="400px"
+                zoom={3}
+                center={{ lat: 22.54992, lng: 0 }}
+                gestureHandling={'greedy'}
+                disableDefaultUI={true}
+            />
+        </APIProvider>
+    );
 
-const center = {
-    lat: -3.745,
-    lng: -38.523
-};
-
-function BasicMap() {
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "YOUR_API_KEY"
-    })
-
-    const [map, setMap] = React.useState(null)
-
-    const onLoad = React.useCallback(function callback(map) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
-        const bounds = new window.google.maps.LatLngBounds(center);
-        map.fitBounds(bounds);
-
-        setMap(map)
-    }, [])
-
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null)
-    }, [])
-
-    return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-        >
-            { /* Child components, such as markers, info windows, etc. */ }
-            <></>
-        </GoogleMap>
-    ) : <></>
 }
-
-export default React.memo(BasicMap)
+function map(){
+    return(
+        badMap()
+    )
+}
+export default map
