35d952c45af17183c260ae0ae14818e965343757
Peter Galati <peter8galati@gmail.com>
2024-02-03 16:36:21 +0000

    basic db setup

---
+++ backend/firebase/query.js
@@ -0,0 +1,3 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc, doc, setDoc } from "firebase/firestore";
+
---
+++ backend/firebase/setup.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRGspZg8-hlhgEKXsJXfsbX4Smw7IcAd0",
+  authDomain: "get-in-the-zone-ich24.firebaseapp.com",
+  projectId: "get-in-the-zone-ich24",
+  storageBucket: "get-in-the-zone-ich24.appspot.com",
+  messagingSenderId: "972436340877",
+  appId: "1:972436340877:web:47963146038ae0f8ae62c4",
+  measurementId: "G-JX0610TP28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
