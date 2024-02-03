c4944f0b04ee2218dff2af0835caad12b68b68ca
os22128 <os22128@bristol.ac.uk>
2024-02-03 15:07:07 +0000

    creating inital app

---
+++ git-zone/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicMap from "./components/map";
+
+function App() {
+  return (
+    <div className="App">
+        {BasicMap}
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+          GiT-zone
+
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
