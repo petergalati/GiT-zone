d2d0dc160b3b95768feafc9eee2e12929aa3bda5
os22128 <os22128@bristol.ac.uk>
2024-02-03 18:01:36 +0000

    markers kinda work on click, dont display though

--- git-zone/src/App.js
+++ git-zone/src/App.js
@@ -8,15 +8,16 @@ function App() {
     const badmap = badMap()
     return (
         <div className="App">
-            <div style={{ height: '50vh', width: '25%' }}>
-                {badmap}
-            </div>
+
 
             <header className="App-header">
                 <p>
                     GiT-zone
                 </p>
             </header>
+            <div style={{ height: '75vh', width: '75%' }}>
+            {badmap}
+        </div>
         </div>
     );
 }
--- git-zone/src/components/map.jsx
+++ git-zone/src/components/map.jsx
@@ -1,23 +1,73 @@
-import {APIProvider, Map} from "@vis.gl/react-google-maps";
+import {APIProvider, Map, Marker} from "@vis.gl/react-google-maps";
+import { useState} from "react";
+const nullMarker =[
+    {
+    position:{lat: 51.507351, lng: -0.127758 },
+    clickable :true,
+    title:'penis'
+},    {
+        position:{lat: 51.607351, lng: -0.127758 },
+        clickable :true,
+        title:'penis2'
+    }]
 
-const badMap = () => {
-    return (
+
+
+const BadMap = () => {
+    const [markerList, setMarkerList] = useState(nullMarker);
+    const [mapKey, setMapKey] = useState(1);
+
+    const handleMapClick = (mapsMouseEvent) => {
+        const newMarker = {
+            position: mapsMouseEvent.latLng,
+            clickable: true,
+            title: "New Marker"
+        };
+        setMapKey((prevKey) => prevKey + 1);
+        setMarkerList((prevMarkers) => [...prevMarkers, newMarker]);
+    };
+
+
+    return (<div style={{ height: '75vh', width: '75%' }}>
         <APIProvider apiKey={"AIzaSyDVGnLbmVRcZ9s82BWKTJ01SipwMv9fDQU"}>
             <Map
+                key={mapKey}
                 width="100%"
                 height="400px"
-                zoom={3}
-                center={{ lat: 22.54992, lng: 0 }}
+                zoom={15}
+                center={{lat: 51.507351, lng: -0.127758}}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
-            />
-        </APIProvider>
-    );
+                onClick={handleMapClick}
 
+            >
+
+                {markerList.map((marker, index) => (
+                    <Marker
+                        key={index}
+                        position={marker.position}
+                        clickable={marker.clickable}
+                        title={marker.title}
+                    />
+
+                ))}
+
+            </Map>
+        </APIProvider>
+            {markerList.map((marker,index)=>(
+                <div>{index}{marker.title}</div>
+                ))
+
+            }
+    </div>
+    );
 }
-function map(){
-    return(
-        badMap()
+
+function map() {
+
+    return (
+        BadMap()
     )
 }
+
 export default map
