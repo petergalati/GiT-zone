3d6497b2fcca3315093ea3e8fd31b9a2ec88d2f1
Dhillon <dhillon.thu@gmail.com>
2024-02-03 18:26:48 +0000

    Added JSON firestore key 2

--- backend/backend.py
+++ backend/backend.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, HTTPException
+from google.cloud import firestore
+#from fastapi.request import 
+from typing import List
+import requests
 
 app = FastAPI()
 
-@app.get("/assign-zone")
-def read_root():
-    return {"message": "Zone assigned successfully"}
+dev_id = 'getinthezone-prod-QNQC2ozrna'
+
+api_key = ''
+
+# Initialize Firestore client
+db = firestore.Client.from_service_account_json('get-in-the-zone-ich24-firebase-adminsdk-78vgj-3090d99d6e.json')
+
+# Function to fetch zones from Firestore
+def get_zones_from_firestore() -> List[dict]:
+    zones_collection = db.collection('zones')
+    zones = [zone.to_dict() for zone in zones_collection.stream()]
+    return zones
+
+@app.get('/login')
+async def auth():
+    res = requests.post('https://api.tryterra.co/auth/generateWidgetSession', headers={'dev-id': dev_id, 'x-api-key': api_key})
+    data = res.json()
+    url = data['url']
+
+@app.get("/zones", response_model=List[dict])
+async def get_zones():
+    zones = get_zones_from_firestore()
+    return zones
+
+
+
